1a5e6fb1f010d56f5ab237a4d2500d2fa6dab066
amish <pithvaamish76@gmail.com>
2025-02-14 16:10:56 +0530

    component lib added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@mui/material'
 
 const Navbar = () => {
+
   return (
-    <div>Navbar</div>
+    <div>
+      Navbar
+    </div>
   )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), visualizer(), tailwindcss()],
+  build: {
+    minify: "esbuild", // Ensure ESBuild is used for minification
+  },
+});
