bdbeb44ee4e8665934e4dc488e360bbb8100e1a9
amish <pithvaamish76@gmail.com>
2025-02-14 12:41:02 +0530

    basic react init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import AppLayout from './layout/AppLayout.jsx'
+import { Home, Contact, About } from './pages/index.js'
 
 function App() {
-
-  
   return (
-    <div>
-
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<AppLayout />}>
+          <Route path='/' index element={<Home />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='about' element={<About />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+import Home from "./Home.jsx";
+import Contact from "./Contact.jsx";
+import About from "./About.jsx";
+
+export { Home, Contact, About };
