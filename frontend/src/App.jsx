1b1838e4bf19aaabba099309c700d866f7908a5b
amish <pithvaamish76@gmail.com>
2025-02-14 13:31:30 +0530

    project initialize with basic layout & admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,30 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import AppLayout from './layout/AppLayout.jsx'
-import { Home, Contact, About } from './pages/index.js'
+import AdminLayout from './layout/AdminLayout.jsx'
+import { Home, Contact, About, Signup, Login } from './pages/index.js'
+import AuthWrapper from './utils/AuthWrapper.jsx'
+import { AdminLogin } from './pages/admin/index.admin.js'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<AppLayout />}>
+        {/* User Routes */}
+        <Route path='/' element={<AuthWrapper redirect='/auth/login' isAuthenticated={true}><AppLayout /></AuthWrapper>}>
           <Route path='/' index element={<Home />} />
           <Route path='contact' element={<Contact />} />
           <Route path='about' element={<About />} />
         </Route>
+
+        {/* Admin Routes */}
+        <Route path='/admin' element={<AuthWrapper redirect='/auth/admin' isAdmin={true}><AdminLayout /></AuthWrapper>}>
+        </Route>
+
+        {/* Auth Routes */}
+        <Route path='/auth/admin' element={<AdminLogin />} />
+        <Route path='/auth/signup' element={<Signup />} />
+        <Route path='/auth/login' element={<Login />} />
       </Routes>
     </>
   )
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,7 @@
 import Home from "./Home.jsx";
 import Contact from "./Contact.jsx";
 import About from "./About.jsx";
+import Login from "./Login.jsx";
+import Signup from "./Signup.jsx";
 
-export { Home, Contact, About };
+export { Home, Contact, About, Login, Signup };
